3beaafb1912541574988590c5843ccc797baf970
gabriel leão <gabidonn@gmail.com>
2023-11-25 01:17:16 -0300

    ajustes

--- index.php
+++ index.php
@@ -5,23 +5,29 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>cassino predict</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link href="main.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+  <script src="main.js"></script>
 </head>
 
 <body class="bg-light text-dark">
 
   <div class="container my-4">
     <div class="d-flex justify-content-between align-items-center mb-4">
-      <div id="timer" class="h2 font-weight-bold">TIMER: 00:00</div>
-      <div id="number" class="h2 font-weight-bold">NUMBER: -</div>
+      <div id="timer" class="h2 font-weight-bold">Timer: 00:00</div>
+      <div hidden id="number" class="h2 font-weight-bold">Último Número: -</div>
     </div>
 
+    <div class="d-flex justify-content-center align-items-center mb-4">
+      <div id="sortedNumber" class="h2 circulo-2"></div>
+    </div>
+    
     <div class="d-flex justify-content-center mb-4">
-      <button id="startButton" class="btn btn-primary">COMEÇAR</button>
-      <button id="pauseButton" class="btn btn-danger ms-2" disabled>PAUSAR</button>
+      <button id="startButton" class="btn btn-primary" onclick="start();">COMEÇAR</button>
+      <button id="pauseButton" class="btn btn-danger ms-2" disabled onclick="stop();">PAUSAR</button>
     </div>
 
     <div id="history" class="d-flex justify-content-center mb-4"></div>
@@ -32,80 +38,10 @@
   </div>
 
   <script>
-    let intervaloTempo;
-    let segundosPassados = 0;
-    let isPausado = true;
-    let corValor = {'branco': 0, 'vermelho': 0, 'preto': 0 };
-    let quantidadeNumeros = 21;
+    updateTimer();
 
-    const updateTimer = () => {
-      const minutos = Math.floor(segundosPassados / 60).toString().padStart(2, '0');
-      const segundos = (segundosPassados % 60).toString().padStart(2, '0');
-      $('#timer').text(`TIMER: ${minutos}:${segundos}`);
-    };
-
-    const updateGraficoPizza = () => {
-      $.each(corValor, function (cor, quantidade) {
-        if(cor == 'branco'){
-          pieChart.data.datasets[0].data[0] = quantidade;
-        }
-        if(cor == 'vermelho'){
-          pieChart.data.datasets[0].data[1] = quantidade;
-        }
-        if(cor == 'preto'){
-          pieChart.data.datasets[0].data[2] = quantidade;
-        }
-        pieChart.update();
-      });
-    };
-
-    const sortearNumero = () => {
-      const number = Math.floor(Math.random() * 22);
-      var cor = 'branco';
-
-      if (number > 0 && number <= 11) {
-        cor = 'vermelho';
-      } else if (number > 0 && number > 11) {
-        cor = 'preto';
-      }
-
-      corValor[cor]++;
-
-      console.log(corValor);
-      updateGraficoPizza();
-      $('#number').text(`NUMBER: ${number}`);
-      const circle = $('<div>').addClass('circle ' + cor).text(number);
-      $('#history').append(circle);
-    };
-
-    $('#startButton').on('click', function () {
-      $('#startButton').html("RESUMIR");
-      $('#startButton').prop("disabled", true);
-      $('#pauseButton').prop("disabled", false);
-
-      if (isPausado) {
-        isPausado = false;
-        intervaloTempo = setInterval(() => {
-          segundosPassados++;
-          updateTimer();
-          if (segundosPassados % 1 === 0) {
-            sortearNumero();
-            segundosPassados = 0;
-          }
-        }, 1000);
-      }
-    });
-
-    $('#pauseButton').on('click', function () {
-      $('#startButton').prop("disabled", false);
-      $('#pauseButton').prop("disabled", true);
-
-      clearInterval(intervaloTempo);
-      isPausado = true;
-      updateTimer();
-    });
-
-    let pieChart = new Chart($('#pieChart'), {
+    //inicia o chart
+    var pieChart = new Chart($('#pieChart'), {
       type: 'pie',
       data: {
         labels: ['branco', 'vermelho', 'preto'],
@@ -121,8 +57,6 @@
         maintainAspectRatio: false
       }
     });
-
-    updateTimer();
   </script>
 
 </body>
--- main.js
+++ main.js
@@ -1,66 +1,72 @@
-let timerInterval;
-let secondsElapsed = 0;
-let isPaused = true;
-let numberCounts = { 0: 0, 1: 0, 2: 0 };
+var intervaloTempo;
+var segundosPassados = 0;
+var isPausado = true;
+var corValor = { 'branco': 0, 'vermelho': 0, 'preto': 0 };
+var quantidadeNumeros = 21;
 
-const updateTimerDisplay = () => {
-    const minutes = Math.floor(secondsElapsed / 60).toString().padStart(2, '0');
-    const seconds = (secondsElapsed % 60).toString().padStart(2, '0');
-    $('#timer').text(`TIMER: ${minutes}:${seconds}`);
-};
+function updateTimer() {
+    const minutos = Math.floor(segundosPassados / 60).toString().padStart(2, '0');
+    const segundos = (segundosPassados % 60).toString().padStart(2, '0');
+    $('#timer').text(`Timer: ${minutos}:${segundos}`);
+}
 
-const updatePieChart = () => {
-    pieChart.data.datasets[0].data = [numberCounts[0], numberCounts[1], numberCounts[2]];
-    pieChart.update();
-};
-
-const drawNumber = () => {
-    const number = Math.floor(Math.random() * 3);
-    numberCounts[number]++;
-    updatePieChart();
-    $('#number').text(`NUMBER: ${number}`);
-    const circle = $('<div>').addClass('circle').text(number);
-    $('#history').append(circle);
-};
-
-$('#startButton').on('click', function () {
-    if (isPaused) {
-        isPaused = false;
-        if (!timerInterval) {
-            timerInterval = setInterval(() => {
-                secondsElapsed++;
-                updateTimerDisplay();
-                if (secondsElapsed % 10 === 0) {
-                    drawNumber();
-                }
-            }, 1000);
+function updateGraficoPizza() {
+    $.each(corValor, function (cor, quantidade) {
+        if (cor == 'branco') {
+            pieChart.data.datasets[0].data[0] = quantidade;
         }
+        if (cor == 'vermelho') {
+            pieChart.data.datasets[0].data[1] = quantidade;
+        }
+        if (cor == 'preto') {
+            pieChart.data.datasets[0].data[2] = quantidade;
+        }
+        pieChart.update();
+    });
+}
+
+function sortearNumero() {
+    const number = Math.floor(Math.random() * 22);
+    var cor = 'branco';
+
+    if (number > 0 && number <= 11) {
+        cor = 'vermelho';
+    } else if (number > 0 && number > 11) {
+        cor = 'preto';
     }
-});
 
-$('#pauseButton').on('click', function () {
-    clearInterval(timerInterval);
-    timerInterval = null;
-    isPaused = true;
-    secondsElapsed = 0;
-    updateTimerDisplay();
-});
+    corValor[cor]++;
 
-let pieChart = new Chart($('#pieChart'), {
-    type: 'pie',
-    data: {
-        labels: ['0', '1', '2'],
-        datasets: [{
-            data: [numberCounts[0], numberCounts[1], numberCounts[2]],
-            backgroundColor: ['#f87171', '#fbbf24', '#60a5fa'],
-            borderColor: ['#f87171', '#fbbf24', '#60a5fa'],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false
+    updateGraficoPizza();
+    const circulo = $('<div>').addClass('circulo ' + cor).text(number);
+    $('#number').text(`Último Número: ${number}`);
+    $('#sortedNumber').text(number).addClass('circulo ' + cor);
+    $('#history').append(circulo);
+}
+
+function stop() {
+    $('#startButton').prop("disabled", false);
+    $('#pauseButton').prop("disabled", true);
+
+    clearInterval(intervaloTempo);
+    isPausado = true;
+    updateTimer();
+}
+
+function start() {
+    $('#startButton').html("RESUMIR");
+    $('#startButton').prop("disabled", true);
+    $('#pauseButton').prop("disabled", false);
+
+    if (isPausado) {
+        isPausado = false;
+        intervaloTempo = setInterval(() => {
+            segundosPassados++;
+            updateTimer();
+            if (segundosPassados % 5 === 0) {
+                sortearNumero();
+                segundosPassados = 0;
+            }
+        }, 1000);
     }
-});
-
-updateTimerDisplay();
+}
